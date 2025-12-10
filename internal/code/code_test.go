cf99883d569834d69c0bcf9c7a422876711aa433
apavlov <apavlov@igatec.com>
2025-12-10 20:15:52 +0100

    tests have been reworked

--- internal/code/code_test.go
+++ internal/code/code_test.go
@@ -1,51 +1,117 @@
 package code
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetSize(t *testing.T) {
-	r := require.New(t)
-	first := "./testdata/file1.txt"
-	second := "./testdata/2"
-	third := "./testdata/2/file2.txt"
-	fourth := "./testdata/bigFile.txt"
+	tests := []struct {
+		name  string
+		path  string
+		human bool
+		all   bool
+		want  string
+	}{
+		{
+			name:  "regular file",
+			path:  "./testdata/file1.txt",
+			human: false,
+			all:   false,
+			want:  "9B\t./testdata/file1.txt",
+		},
+		{
+			name:  "directory",
+			path:  "./testdata/2",
+			human: false,
+			all:   false,
+			want:  "48B\t./testdata/2",
+		},
+		{
+			name:  "nested file",
+			path:  "./testdata/2/file2.txt",
+			human: false,
+			all:   false,
+			want:  "24B\t./testdata/2/file2.txt",
+		},
+		{
+			name:  "human-readable",
+			path:  "./testdata/bigFile.txt",
+			human: true,
+			all:   false,
+			want:  "9.5KB\t./testdata/bigFile.txt",
+		},
+		{
+			name:  "hidden file excluded",
+			path:  "./testdata/.secret",
+			human: true,
+			all:   false,
+			want:  "0B\t./testdata/.secret",
+		},
+		{
+			name:  "hidden file included",
+			path:  "./testdata/.secret",
+			human: true,
+			all:   true,
+			want:  "200.0KB\t./testdata/.secret",
+		},
+	}
 
-	res1, err := GetSize(first, false)
-	r.NoError(err)
-	r.Equal(fmt.Sprintf("9B\t%s", first), res1)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := require.New(t)
 
-	res2, err := GetSize(second, false)
-	r.NoError(err)
-	r.Equal(fmt.Sprintf("48B\t%s", second), res2)
-
-	res3, err := GetSize(third, false)
-	r.NoError(err)
-	r.Equal(fmt.Sprintf("24B\t%s", third), res3)
-
-	res4, err := GetSize(fourth, true)
-	r.NoError(err)
-	r.Equal(fmt.Sprintf("9.5KB\t%s", fourth), res4)
+			got, err := GetSize(tt.path, tt.human, tt.all)
+			r.NoError(err)
+			r.Equal(tt.want, got)
+		})
+	}
 }
 
 func TestFormatSize(t *testing.T) {
-	r := require.New(t)
-	res1, err := FormatSize(1234567, true)
-	r.NoError(err)
-	r.Equal(res1, "1.2MB")
+	tests := []struct {
+		name  string
+		size  int64
+		human bool
+		want  string
+		err   bool
+	}{
+		{
+			name:  "human bytes",
+			size:  123,
+			human: true,
+			want:  "123B",
+		}, {
+			name:  "human MB",
+			size:  25165824,
+			human: true,
+			want:  "24.0MB",
+		},
+		{
+			name:  "negative size",
+			size:  -1,
+			human: true,
+			err:   true,
+		},
+		{
+			name:  "raw bytes",
+			size:  25165824,
+			human: false,
+			want:  "25165824B",
+		},
+	}
 
-	res2, err := FormatSize(123, true)
-	r.NoError(err)
-	r.Equal(res2, "123B")
-
-	res3, _ := FormatSize(-1, true)
-	r.NoError(err)
-	r.Equal(res3, "")
-
-	res4, err := FormatSize(25165824, false)
-	r.NoError(err)
-	r.Equal(res4, "25165824B")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := require.New(t)
+			got, err := FormatSize(tt.size, tt.human)
+			if tt.err {
+				r.Error(err)
+				return
+			}
+			r.NoError(err)
+			r.Equal(tt.want, got)
+		})
+	}
 }
